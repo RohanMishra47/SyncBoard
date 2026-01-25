caa85aa0cd53708db3d48419621dad088baf1abb
RohanMishra47 <mydearluffy093@gmail.com>
2026-01-25 13:35:56 +0530

    feat: update Prisma schema output path and add prisma.config.ts file into src folder

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,6 +1,6 @@
+import { PrismaClient } from "@/generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
-import { PrismaClient } from "generated/prisma/client.js";
 import pg from "pg";
 
 const pool = new pg.Pool({
